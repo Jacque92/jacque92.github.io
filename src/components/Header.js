b7d58c279dbc93ecc15cfb33a0158b26c63f9600
jacque <jacquelinezhang92@gmail.com>
2022-01-26 14:40:58 +1000

    debug

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
+import logo from "../images/logo.png";
+import linkedin from "../images/linkedin.png";
+import send from "../images/send.png";
+
 import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <header className="header">
       <Link to="/">
-        <img
-          src="../../public/images/logo.png"
-          alt="logo"
-          height="40"
-          width="40"
-        ></img>
+        <img src={logo} alt="logo" height="40" width="40"></img>
       </Link>
       <nav>
         <ul>
@@ -33,20 +32,10 @@ export const Header = () => {
         </ul>
         <div className="socialLink">
           <a href="https://www.linkedin.com/in/tiexin-zhang-480283190/">
-            <img
-              src="../../public/images/linkedin.png"
-              alt="linkedin"
-              height="28"
-              width="28"
-            ></img>
+            <img src={linkedin} alt="linkedin" height="28" width="28"></img>
           </a>
           <a href="mailto:jacquelinezhang92@gmail.com">
-            <img
-              src="../../public/images/send.png"
-              alt="send"
-              height="28"
-              width="28"
-            ></img>
+            <img src={send} alt="send" height="28" width="28"></img>
           </a>
         </div>
       </nav>
